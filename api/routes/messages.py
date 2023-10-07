0470b5c93600efc9bd6ccb95f2a5c69f17599ee9
VincentPro <145966298+GitHubVincentPro@users.noreply.github.com>
2023-10-07 04:20:26 +0200

    v2

--- api/routes/messages.py
+++ api/routes/messages.py
@@ -1,7 +1,49 @@
-from fastapi import APIRouter
+python
+from fastapi import APIRouter, Body
+from app.models import Message
+from app.db import messages_collection
 
 router = APIRouter()
 
+@router.post("/")
+async def create_message(message: Message = Body(...)):
+
+# Validate message
+
+result = await messages_collection.insert_one(message.dict())
+
+# Handle possible errors
+
+return {"message": "Message créé avec succès"}
+
 @router.get("/")
-async def root():
-return {"message": "Hello from messages route"}
+async def get_messages():
+
+messages = []
+
+cursor = messages_collection.find()
+
+for doc in await cursor.to_list(length=100):
+
+message = Message(**doc)
+
+# Validate message
+
+messages.append(message)
+
+return messages
+
+@router.get("/{message_id}")
+async def get_message(message_id: str):
+
+document = await messages_collection.find_one({"_id": message_id})
+
+if document:
+
+message = Message(**document)
+
+# Validate message
+
+return message
+
+return {"error": "Message non trouvé"}
